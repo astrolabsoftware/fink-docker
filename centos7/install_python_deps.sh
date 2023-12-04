2544c15ac51bbe808b3bc02806caeedd9c38f9ea
JulienPeloton <peloton@lal.in2p3.fr>
2023-12-04 21:02:06 +0100

    typo

--- centos7/install_python_deps.sh
+++ centos7/install_python_deps.sh
@@ -24,7 +24,7 @@ Usage:\n
 pip install --no-cache-dir -r requirements.txt
 
 # Fink-fat
-pip install --no-dependencies git+https://github.com/FusRoman/fink-fat/archive/v0.16.0.zip
+pip install --no-dependencies https://github.com/FusRoman/fink-fat/archive/v0.16.0.zip
 
 # Fink_MM
 pip install --no-dependencies https://github.com/FusRoman/Fink_MM/archive/v0.16.10.zip
